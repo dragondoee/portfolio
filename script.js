0eb9d3b0d0b2586e21fcb5ff3e3230cef0510962
Meylie <emilie.desgranges78@gmail.com>
2024-12-30 20:06:37 +0100

    pop up fermeture

--- script.js
+++ script.js
@@ -6,15 +6,51 @@ const popup = document.querySelector(".popup")
 const fermer = document.querySelector(".fermer")
 const content = document.querySelector(".content-popup")
 
+fetch('projets/projet.json').then(function (response) {
+    response.json().then(function (data) {
+
+        function popupProjet(){
+            content.innerHTML = `
+                <h3>${data[0].title}</h3>
+                <div class="popup-bandeau">
+                    <p>${data[0].end_date}</p>
+                    <p>${data[0].team_size}</p>
+                    <p>${data[0].development_type}</p>
+                </div>
+                <div class="detail">
+                    <img class="img-album" src="${data[0].image}" alt="">
+                    <p>${data[0].description}</p>
+                </div>
+                <a href="${data[0].link}" class="" target="_blank"> Voir le site <a>
+            `
+        }
+            
+        
+        
+        
+        
+        projets.forEach(function (projet) {
+            projet.addEventListener("click", function () {
+                popupProjet()
+                popup.style.display = "block"
+                console.log(content)
+                console.log(fermer )
+                
+            });
+        });
+        
+
+        fermer.addEventListener("click", function () {
+            console.log("Bouton fermer cliqué !");
+            popup.style.display = "none";
+        });
+        
+        popup.addEventListener("click", function (e) {
+            if (!content.contains(e.target)) {
+                popup.style.display = "none";
+            }
+        });
 
 
-
-projets.forEach(function (projet) {
-    projet.addEventListener("click", function () { 
-        popup.style.display="block"
     });
-});
-
-popup.addEventListener("click", function () { 
-    popup.style.display="none"
 });
